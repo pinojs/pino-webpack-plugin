64b1e4c7a5fbf56c808cf9cbcd50a3a50d54275c
Guido D'Orsi <guido.dorsi@nearform.com>
2021-12-15 13:50:35 +0100

    test: adds a test for the development mode

---
+++ test/development-mode.test.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const { spawnSync } = require('child_process')
+const { resolve } = require('path')
+const { test } = require('tap')
+const webpack = require('webpack')
+const { PinoWebpackPlugin } = require('../src/index')
+const execa = require('execa')
+
+function runBuild(distFolder, onDone) {
+  webpack(
+    {
+      context: resolve(__dirname, 'fixtures'),
+      mode: 'development',
+      target: 'node',
+      entry: {
+        main: './second.js'
+      },
+      output: {
+        path: distFolder
+      },
+      plugins: [new PinoWebpackPlugin({ transports: ['pino-pretty'] })]
+    },
+    onDone
+  )
+}
+
+test('should work in webpack development mode', (t) => {
+  t.plan(3)
+
+  const distFolder = resolve(__dirname, '../tmp/pino-transport-bug')
+
+  t.teardown(() => {
+    spawnSync(`rm -rf ${distFolder}`, {
+      shell: true
+    })
+  })
+
+  runBuild(distFolder, async (err, stats) => {
+    t.error(err)
+    t.notOk(stats.hasErrors())
+
+    const { stdout } = await execa(process.argv[0], [resolve(distFolder, 'main.js')])
+
+    t.match(stdout, /This is second!/)
+  })
+})
