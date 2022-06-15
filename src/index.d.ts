c46b12c184c09bf7bda46434fa01ced4a937fdfb
Segev Finer <segev208@gmail.com>
2022-06-16 02:44:16 +0300

    Add tests

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,7 +4,7 @@ export const banner: string;
 export const footer: string;
 
 export class PinoWebpackPlugin implements WebpackPluginInstance {
-    constructor(options: {transports: string[]});
-    [index: string]: any;
-    apply: (compiler: Compiler) => void;
+  constructor(options: {transports: string[]});
+  [index: string]: any;
+  apply: (compiler: Compiler) => void;
 }
