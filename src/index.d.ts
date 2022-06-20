446c8f2027726c8d4dcd4f83fd31a8c64a097e17
Segev Finer <segev208@gmail.com>
2022-06-20 18:14:12 +0300

    Add TypeScript typings (#44)
    * Add TypeScript typings
    
    * Add tests
    
    * Fix test:types invocation in package.json
    
    * Fix test:ci
    
    Guess some unfortunate word wrapping made me miss that

---
+++ src/index.d.ts
@@ -0,0 +1,10 @@
+import { Compiler, WebpackPluginInstance } from 'webpack';
+
+export const banner: string;
+export const footer: string;
+
+export class PinoWebpackPlugin implements WebpackPluginInstance {
+  constructor(options: {transports: string[]});
+  [index: string]: any;
+  apply: (compiler: Compiler) => void;
+}
---
+++ test/types/index.test-d.ts
@@ -0,0 +1,9 @@
+import { expectType } from "tsd";
+import { WebpackPluginInstance } from "webpack";
+import { PinoWebpackPlugin, banner, footer } from "../..";
+
+expectType<WebpackPluginInstance>(new PinoWebpackPlugin({
+  transports: ['pino-pretty'],
+}));
+expectType<string>(banner);
+expectType<string>(footer);
